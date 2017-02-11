a91f1fdcc1c05eb9e566a2a655ac4e24bbc22813
René Fouquet <mail@fouquet.me>
2017-02-11 13:09:14 +0100

    Removed supports for all but camera and photos

--- PermissionScope/Permissions.swift
+++ PermissionScope/Permissions.swift
@@ -7,15 +7,8 @@
 //
 
 import Foundation
-import CoreLocation
-import AddressBook
 import AVFoundation
 import Photos
-import EventKit
-import CoreBluetooth
-import CoreMotion
-import CloudKit
-import Accounts
 
 /**
 *  Protocol for permission configurations.
@@ -34,28 +27,8 @@ import Accounts
     }
 }
 
-@objc public class LocationWhileInUsePermission: NSObject, Permission {
-    public let type: PermissionType = .locationInUse
-}
-
-@objc public class LocationAlwaysPermission: NSObject, Permission {
-    public let type: PermissionType = .locationAlways
-}
-
-@objc public class ContactsPermission: NSObject, Permission {
-    public let type: PermissionType = .contacts
-}
-
 public typealias requestPermissionUnknownResult = () -> Void
 public typealias requestPermissionShowAlert     = (PermissionType) -> Void
-
-@objc public class EventsPermission: NSObject, Permission {
-    public let type: PermissionType = .events
-}
-
-@objc public class MicrophonePermission: NSObject, Permission {
-    public let type: PermissionType = .microphone
-}
 
 @objc public class CameraPermission: NSObject, Permission {
     public let type: PermissionType = .camera
@@ -63,16 +36,4 @@ public typealias requestPermissionShowAlert     = (PermissionType) -> Void
 
 @objc public class PhotosPermission: NSObject, Permission {
     public let type: PermissionType = .photos
-}
-
-@objc public class RemindersPermission: NSObject, Permission {
-    public let type: PermissionType = .reminders
-}
-
-@objc public class BluetoothPermission: NSObject, Permission {
-    public let type: PermissionType = .bluetooth
-}
-
-@objc public class MotionPermission: NSObject, Permission {
-    public let type: PermissionType = .motion
 }
--- PermissionScope/Structs.swift
+++ PermissionScope/Structs.swift
@@ -10,12 +10,10 @@ import Foundation
 
 /// Permissions currently supportes by PermissionScope
 @objc public enum PermissionType: Int, CustomStringConvertible {
-    case contacts, locationAlways, locationInUse, notifications, microphone, camera, photos, reminders, events, bluetooth, motion
+    case notifications, camera, photos
     
     public var prettyDescription: String {
         switch self {
-        case .locationAlways, .locationInUse:
-            return "Location"
         default:
             return "\(self)"
         }
@@ -23,21 +21,13 @@ import Foundation
     
     public var description: String {
         switch self {
-        case .contacts:         return "Contacts"
-        case .events:           return "Events"
-        case .locationAlways:   return "LocationAlways"
-        case .locationInUse:    return "LocationInUse"
         case .notifications:    return "Notifications"
-        case .microphone:       return "Microphone"
         case .camera:           return "Camera"
         case .photos:           return "Photos"
-        case .reminders:        return "Reminders"
-        case .bluetooth:        return "Bluetooth"
-        case .motion:           return "Motion"
         }
     }
     
-    static let allValues = [contacts, locationAlways, locationInUse, notifications, microphone, camera, photos, reminders, events, bluetooth, motion]
+    static let allValues = [notifications, camera, photos]
 }
 
 /// Possible statuses for a permission.
